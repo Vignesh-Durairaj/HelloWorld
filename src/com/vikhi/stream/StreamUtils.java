2a6e414373097ead76931e1c160c5a37d3858e30
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-02-05 16:38:33 +0530

    Code clean Up

--- src/com/vikhi/stream/StreamUtils.java
+++ src/com/vikhi/stream/StreamUtils.java
@@ -15,15 +15,16 @@ public class StreamUtils {
 	
 	public PerformOperation isPrime() {
 	    return (a -> a == 2 || (a % 2 != 0 && LongStream
-	       .range(2, (long)(Math.sqrt(a)) + 1)
-	       .filter(n -> n % 2 != 0)
-	       .noneMatch(n -> a/n == 0)));
+	    		.range(2, (long)(Math.sqrt(a)) + 1)
+	    		.filter(n -> n % 2 != 0)
+	    		.noneMatch(n -> a/n == 0)));
 	}
 	
 	public PerformOperation isPalindrome() {
-	    return (a -> a == IntStream.iterate(a, i -> i / 10)
-	       .map(n -> n % 10)
-	       .reduce(0, (i, j) -> i * 10 + j));
+	    return (a -> a == IntStream
+	    					.iterate(a, i -> i / 10)
+	    					.map(n -> n % 10)
+	    					.reduce(0, (i, j) -> i * 10 + j));
 	}
 	
 }
