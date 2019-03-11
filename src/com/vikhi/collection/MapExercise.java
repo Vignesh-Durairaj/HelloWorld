22d10705068c557bf82aeadaff5c03dfc32ce422
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-03-11 14:53:31 +0530

    Stream based method to calculate the character frequency in a word

--- src/com/vikhi/collection/MapExercise.java
+++ src/com/vikhi/collection/MapExercise.java
@@ -1,11 +1,14 @@
 package com.vikhi.collection;
 
+import java.nio.CharBuffer;
 import java.util.AbstractMap;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class MapExercise {
 
@@ -24,5 +27,23 @@ public class MapExercise {
 			.forEach(ele -> myMap.merge(ele, 1, (v1, v2) -> v1 + 1));
 		
 		System.out.println(myMap);
+		
+		getCharacterCount("This is sparta");
+	}
+	
+	private static void getCharacterCount(final String str) {
+		if (str == null || str.trim().length() == 0) {
+			throw new IllegalArgumentException("The input string should not be empty or null");
+		}
+		
+		Map<Character, Integer> charCountMap = new HashMap<>();
+		
+		CharBuffer
+			.wrap(str.replace(" ", "").toLowerCase().toCharArray())
+			.chars()
+			.mapToObj(charInt -> (char)charInt)
+			.forEach(ch -> charCountMap.merge(ch, 1, (v1, v2) -> v1 + 1));
+		
+		System.out.println(charCountMap);
 	}
 }
