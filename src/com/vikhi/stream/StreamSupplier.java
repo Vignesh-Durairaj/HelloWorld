4d7a2081f3c8f6fdd757eeed1da4db0c0f26f907
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-03-12 12:07:57 +0530

    Sample Stream generator using supplier

---
+++ src/com/vikhi/stream/StreamSupplier.java
@@ -0,0 +1,33 @@
+package com.vikhi.stream;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import com.vikhi.stream.model.Employee;
+
+public class StreamSupplier implements Supplier<Stream<Employee>> {
+
+	private Object dataCollection;
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public Stream<Employee> get() {
+		
+		Stream<Employee> employeeStream;
+		
+		if (dataCollection instanceof List<?>) {
+			employeeStream = StreamCreator.createStreamFromList((List<Employee>) dataCollection);
+		} else if (dataCollection.getClass().isArray()) {
+			employeeStream = StreamCreator.createStreamFromArray((Employee[]) dataCollection);
+		} else {
+			employeeStream = Stream.empty();
+		}
+		return employeeStream;
+	}
+
+	public void setDataCollection(Object dataCollection) {
+		this.dataCollection = dataCollection;
+	}
+
+}
